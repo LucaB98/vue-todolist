c7bd97e3e6887ae3c82b1599b5824c3fa19faf1c
LucaB98 <lucabellan98@gmail.com>
2024-01-23 17:08:47 +0100

    added logo and function delete task

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return{
-            message: 'Hello Vue!',
+            tasks:[
+                { id: 1, done: false, text: 'Fare la spesa' },
+                { id: 2, done: true, text: 'Lavare i capelli' },
+                { id: 3, done: false, text: 'Comprare una marca da bollo' },
+                { id: 4, done: false, text: 'Aggiornare il PC' }
+              ],
+            newTask: '',
+        }
+    },
+    methods:{
+        deleteTask(id){
+            this.tasks = this.tasks.filter(task => id !== task.id)
         }
     }
 })
